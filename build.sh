fa758bf4d5abab127c3267becc6c8e5989daf542
Luis Alexandre Deschamps Brandão <techmago@ymail.com>
2018-06-16 11:23:24 -0300

    FIX: Nome de tag errada no repo

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ release=$(cat release)
 echo "Gerando relase: ${release}"
 
 # Gera uma build
-docker build -t rentos:latest -t luisos:${release} .
+docker build -t luisos:latest -t luisos:${release} .
 hash=$(docker images | grep ${release} | awk '{print $3}' | uniq)
 
 # Tagueia a imagem e a salva no registro
